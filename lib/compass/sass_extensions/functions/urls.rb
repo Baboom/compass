0cd2069e3418febbbafc2d20d5876cac0a32e054
Andre Cruz <andremiguelcruz@msn.com>
2012-05-24 12:02:21 +0100

    Generate busting also for absolute paths.

--- lib/compass/sass_extensions/functions/urls.rb
+++ lib/compass/sass_extensions/functions/urls.rb
@@ -87,32 +87,34 @@ module Compass::SassExtensions::Functions::Urls
         # Treat root relative urls (without a protocol) like normal if they start with
         # the images path.
         path = $1
-      elsif absolute_path?(path)
+      elsif absolute_path?(path) && !(path.start_with? '/')
         # Short curcuit if they have provided an absolute url.
         return Sass::Script::String.new("url(#{path})")
       end
 
-      # Compute the path to the image, either root relative or stylesheet relative
-      # or nil if the http_images_path is not set in the configuration.
-      http_images_path = if relative?
-        compute_relative_path(Compass.configuration.images_path)
-      elsif Compass.configuration.http_images_path
-        Compass.configuration.http_images_path
-      else
-        Compass.configuration.http_root_relative(Compass.configuration.images_dir)
-      end
+	  if (!(path.start_with? '/'))
+		  # Compute the path to the image, either root relative or stylesheet relative
+		  # or nil if the http_images_path is not set in the configuration.
+		  http_images_path = if relative?
+			compute_relative_path(Compass.configuration.images_path)
+		  elsif Compass.configuration.http_images_path
+			Compass.configuration.http_images_path
+		  else
+			Compass.configuration.http_root_relative(Compass.configuration.images_dir)
+		  end
 
-      # Compute the real path to the image on the file stystem if the images_dir is set.
-      real_path = if Compass.configuration.images_dir
-        File.join(Compass.configuration.project_path, Compass.configuration.images_dir, path)
-      end
+		  # Compute the real path to the image on the file stystem if the images_dir is set.
+		  real_path = if Compass.configuration.images_dir
+			File.join(Compass.configuration.project_path, Compass.configuration.images_dir, path)
+		  end
 
-      # prepend the path to the image if there's one
-      if http_images_path
-        http_images_path = "#{http_images_path}/" unless http_images_path[-1..-1] == "/"
-        path = "#{http_images_path}#{path}"
-      end
-
+		  # prepend the path to the image if there's one
+		  if http_images_path
+			http_images_path = "#{http_images_path}/" unless http_images_path[-1..-1] == "/"
+			path = "#{http_images_path}#{path}"
+		  end
+	  end
+	  
       # Compute the asset host unless in relative mode.
       asset_host = if !relative? && Compass.configuration.asset_host
         Compass.configuration.asset_host.call(path)
